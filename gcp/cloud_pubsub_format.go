78437ec8340312070b71dc1562cf2eb32a7c6672
Svetlin Ralchev <svetlin@ralch.com>
2021-04-10 12:43:25 +0100

    Improve logging

--- gcp/cloud_pubsub_format.go
+++ gcp/cloud_pubsub_format.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cloudevents/sdk-go/v2/binding/format"
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/cloudevents/sdk-go/v2/types"
+	"github.com/phogolabs/log"
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
@@ -35,11 +36,13 @@ func (PubsubFormat) Unmarshal(data []byte, e *event.Event) error {
 	}
 
 	if err := decoder.Unmarshal(data, payload); err != nil {
+		log.WithError(err).Error("unmarshal payload failure")
 		return err
 	}
 
 	timestamp, err := types.ParseTimestamp(payload.Message.Attributes["ce-time"])
 	if err != nil {
+		log.WithError(err).Error("parse ce-time failure")
 		return err
 	}
 
@@ -47,30 +50,37 @@ func (PubsubFormat) Unmarshal(data []byte, e *event.Event) error {
 	e.DataEncoded = payload.Message.Data
 
 	if err := e.Context.SetID(payload.Message.Attributes["ce-id"]); err != nil {
+		log.WithError(err).Error("set ce-id failure")
 		return err
 	}
 
 	if err := e.Context.SetType(payload.Message.Attributes["ce-type"]); err != nil {
+		log.WithError(err).Error("set ce-type failure")
 		return err
 	}
 
 	if err := e.Context.SetSubject(payload.Message.Attributes["ce-subject"]); err != nil {
+		log.WithError(err).Error("set ce-subject failure")
 		return err
 	}
 
 	if err := e.Context.SetSource(payload.Message.Attributes["ce-source"]); err != nil {
+		log.WithError(err).Error("set ce-source failure")
 		return err
 	}
 
 	if err := e.Context.SetTime(timestamp.Time); err != nil {
+		log.WithError(err).Error("set ce-time failure")
 		return err
 	}
 
 	if err := e.Context.SetDataSchema(payload.Message.Attributes["ce-dataschema"]); err != nil {
+		log.WithError(err).Error("set ce-dataschema failure")
 		return err
 	}
 
 	if err := e.Context.SetDataContentType(payload.Message.Attributes["Content-Type"]); err != nil {
+		log.WithError(err).Error("set content-type failure")
 		return err
 	}
 
